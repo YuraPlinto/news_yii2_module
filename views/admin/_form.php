b2532a22daafab1e65af88eba13d2084df38768f
PlintoYura <plinto.yura@yandex.ru>
2025-01-23 19:45:51 +0600

    Добавлены подписи для полей формы

--- views/admin/_form.php
+++ views/admin/_form.php
@@ -12,15 +12,15 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true])->label('Заголовок') ?>
 
-    <?= $form->field($model, 'img_url')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'img_url')->textInput(['maxlength' => true])->label('URL изображения (относительно папки images)') ?>
 
-    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'content')->textarea(['rows' => 6])->label('Текст статьи') ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+    <?= $form->field($model, 'created_at')->textInput()->label('Дата публикации') ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <?= $form->field($model, 'updated_at')->textInput()->label('Дата изменения') ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
